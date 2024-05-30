b2ea83dbde2745a56b95b70e8cd73b552e83c88b
Imamrozali <muh.imamrozali.git@gmail.com>
2024-05-30 19:09:16 +0700

    update workflow

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,8 +9,14 @@ export default function Home() {
         <title>SWM - Electron App with Next.js 14, TypeScript and PWA</title>
       </Head>
       <div className="min-h-screen bg-gray-900 flex flex-col items-center justify-center">
-        <Image src="/swm-logo.jpeg" alt="logo" width={200} height={200} className="rounded-full" />
-        <h1 className="text-6xl text-white font-bold mb-4 animate-pulse">
+        <Image
+          src="/swm-logo.jpeg"
+          alt="logo"
+          width={200}
+          height={200}
+          className="rounded-full"
+        />
+        <h1 className="text-6xl text-white font-bold mb-4 mt-4 animate-pulse">
           Software Magicians
         </h1>
         <p className="text-white text-lg">
